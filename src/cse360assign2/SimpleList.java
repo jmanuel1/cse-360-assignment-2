d71e7416ec1f9b83e6ed6140a31a135ae2a52ea5
Jason Manuel <jama.indo@hotmail.com>
2020-02-27 12:15:12 -0700

    Expand list capacity when it is exceeded
    Before, the add method would silently drop the last element if the length of the internal array was exceeded. Now, the array is reallocated in such a situation.

--- src/cse360assign2/SimpleList.java
+++ src/cse360assign2/SimpleList.java
@@ -4,6 +4,8 @@
 // Description: A simple array-backed list class which supports adding,
 //  searching for, and removing integers.
 
+package cse360assign2;
+
 /**
  * Implements a list of integers backed by an array. Supports adding and
  * removing integers, searching for the first occurrence of an integer, and
@@ -13,20 +15,24 @@
 public class SimpleList {
     private int[] list;
     private int count;
-    public static final int MAX_COUNT = 10;
+    private static final int INITIAL_CAPACITY = 10;
     public static final int NOT_FOUND = -1;
 
     public SimpleList() {
-        list = new int[MAX_COUNT];
+        list = new int[INITIAL_CAPACITY];
         count = 0;
     }
 
     public void add(int newElement) {
+        if (count == list.length) {
+            int[] copy = new int[(int) (list.length * 1.5)];
+            System.arraycopy(list, 0, copy, 0, list.length);
+            list = copy;
+        }
+
         System.arraycopy(list, 0, list, 1, list.length - 1);
         list[0] = newElement;
-        if (count < list.length) {
-            count++;
-        }
+        count++;
     }
 
     public void remove(int element) {
